6b500ef155c056de30201511e213dc34d95434f8
ioscoh <ssn100407@gmail.com>
2025-04-25 18:37:49 +0900

    베이직 두번째 숙제 제출

--- src/arithmeticTool/Main.java
+++ src/arithmeticTool/Main.java
@@ -10,5 +10,32 @@ public class Main {
         ArithmeticTool arithmeticTool = new ArithmeticTool(1,2,"op");
         ArithmeticTool arithmeticToolA = new ArithmeticTool();
 
+    //1. 기능 활용
+        System.out.println("a와 b를 더합니다.");
+        arithmeticTool.sum(5,5);//매개변수에 인트값을 하나만 넣으니,
+                                 // 위에 정의 된 int 값이 내려와 더해짐
+        arithmeticTool.sud(10);
+
+        arithmeticTool.mul();
+
+        System.out.println("a와 b를 나눕니다.");
+        arithmeticTool.div(10,5);
+    //1-2. 기능 활용_{세터(setter) 활용}
+        arithmeticTool.setA(11);
+        arithmeticTool.setB("+");
+
+    // 1-3. 기능 활용_{게터(getter) 베이직}
+        arithmeticTool.getA();
+        System.out.println(arithmeticTool.getA());
+
+        arithmeticTool.getC();
+        System.out.println(arithmeticTool.getC());
+        //031110이 출력이 안됐던 이유는
+        //앞에 0이 붙으면 8진수라는 수학젹 계념이 작동돼서
+        //031110의 8진수인 12872이 작동 됐던것!
+        //즉 인트에서도 사실 오류난 것이 아니라 잘 계산된거였음
+        //이를 해결하기 위해서 스트링을 쓰면 문제 해결됨.
+        arithmeticTool.getD();
+        System.out.println(arithmeticTool.getD());
     }
 }
--- src/character/Main.java
+++ src/character/Main.java
@@ -1,6 +1,7 @@
 package character;
 
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class Main {
     //속성
@@ -8,10 +9,63 @@ public class Main {
     //생성자
 
     //기능
-  public static void main(String[] args) {
-      Character character = new Character("se",1,"마법사");
-      Character characterA = new Character("gygim");
-      Character characterB = new Character("se","해리포터");
+    public static void main(String[] args) {
+
+        // JVM 메모리 영역 3가지
+        // 1. Stack
+        // 2. Heap
+        // 3. Method Area(static)
+        // 클래스 -> 객체(인스턴스): 객체화(인스턴스화)
+
+        // 객체 생성
+
+
+      Character character = new Character();
+      Character characterA = new Character("se",1,"마법사");
+      Character characterB = new Character("gygim");
+      Character characterC = new Character("se","해리포터");
+      Scanner scanner = new Scanner(System.in);
+        // <기능 활용>
+
+        // HEAP 의 상황...
+        /// ---character-----
+        // - name =
+        // - level =
+        // - jod =
+        /// ---characterA-----
+        // - name = "se"
+        // - level = 1
+        // - jod = "마법사"
+        /// ---characterB-----
+        // - name = "gygim"
+        // - level = 1
+        // - jod = "볼드모트"
+        /// ------------------
+        /// ---characterC-----
+        // - name = "se"
+        // - level = 2
+        // - jod = "해리포터"
+        /// ------------------
+
+        //1. 기능 활용
+
+        //1) 캐락터A가 attack = "이/가 마법을 씁니다." 라는 문구가 메인에 출력 되어 나옴
+        String attack = characterA.playingMagicA("se");
+        System.out.println("attack = " + attack);
+
+        //2) 캐락터A가 리턴 값 없이 "se이/가 마법을 씁니다." 라는 문구가 나옴
+        characterA.playingMagicB();
+
+
+        //1-2. 기능 활용_{세터(setter) 활용}
+
+        characterA.setLevel(8);
+
+
+
+
+
+        //1-3. 기능 활용_{게터(getter) 베이직}
 
   }
 }
